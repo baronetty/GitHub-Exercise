0e9a3f74af3cec9253ed5501d38059dfb8a32b69
Leo Barnett <leo.e.barnett@icloud.com>
2023-11-04 21:34:58 +0200

    Version 1.0 - Red

--- GitHub Exercise/ContentView.swift
+++ GitHub Exercise/ContentView.swift
@@ -10,12 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "1.circle")
+                .resizable().scaledToFit()
+            Text("V. 1.0")
+                .font(.largeTitle)
+                .fontWeight(.black)
         }
         .padding()
+        .foregroundStyle(.red)
     }
 }
 
